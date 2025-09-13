8868f00b00ac1b86708174efa4ae03b6be68df7d
Ved Prajapati <21vedpra@westfield.academy>
2025-09-13 13:06:25 +0000

    Sat, Sep 13, 2025, 2:06 PM +01:00

--- New folder/readability.c
+++ New folder/readability.c
@@ -1,80 +1,92 @@
-#include <stdio.h>
 #include <cs50.h>
-#include <math.h>
-#include <string.h>
 #include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
-    string input = get_string("Text: \n");
+	string text = get_string("Text: ");
+	int letters = 0, words = 0, sentences = 0;
+	float L = 0, S = 0;
+	letters = count_letters(text);
+	words = count_words(text);
+	sentences = count_sentences(text);
+
+	//L is the average number of letters per 100 words in the text.
+	L = (float) letters / (float) words * 100;
+
+	//S is the average number of sentences per 100 words in the text.
+	S = (float) sentences / (float) words * 100;
+
+	float index = 0;
+	index = 0.0588 * L - 0.296 * S - 15.8;
+
+	if (index < 1)
+		printf("Before Grade 1\n");
+	else if (index >= 16)
+		printf("Grade 16+\n");
+	else
+	{
+		printf("Grade %d\n", (int)round(index));
+	}
 
 
-    float letters = 0 ;
-    float words = 1 ;
-    float sentences = 0 ;
-    float index;
-    float L;
-    float S;
+}
 
-    char space = ' ' ;
-    char p = '.' ;
-    char ex = '!' ; 
-    char apos = '\'' ;
-    char ques = '?' ;
-    char apos_2 = '\"';
-    char comma = ',' ;
+int count_letters(string text)
+{
+	int letters = 0;
 
-    for (int i = 0 ; input[i] ; i++)
-    {
-        // count letters
-    if (input[i] != space && input[i] != p && input[i] != ex && input[i] != apos && input[i] != ques && input[i] != apos_2 && input[i] != comma)
-        {
-            letters++ ;
-        }
+	int i = 0;
+	char c = '\0';
+	while (text[i])
+	{
+	 	c = text[i];
+		//check letters to be uppercase or lowercase alphabetical character
+		if (islower(c) || isupper(c))
+			letters++;
+		i++;
+	}
+	return letters;
+}
 
-        // count sentences
-        if (input[i] == p || input[i] == ex || (input[i] == ques && input[i+1] == apos_2) || input[i] == ques)
-        {
-            sentences++ ;
-        }
+int count_words(string text)
+{
+	int words = 1;
 
-        // count words
-        if (input[i] == space && input[i+1] != '\0')
-        {
-            words++ ;
-        }
-    }
-    
-    //letters per 100 words
-    L = (letters / words) * 100 ;
+	int i = 0;
+	char c = '\0';
+	while (text[i])
+	{
+	 	c = text[i];
+		if (c == 32)
+		{
+			//handle multiple spaces between words
+			while (text[i] == 32)
+				i++;
+			words++;
+		}
+		i++;
+	}
+	return words;
+}
 
-    //sentence per 100 words
-    S = (sentences / words) * 100 ;
+int count_sentences(string text)
+{
+	int sentences = 0;
 
-    // total
-    index = (0.0588 * L) - (0.296 * S) - 15.8 ;
-    int f_index = round(index);
-    printf("%f \n", index) ;
-    printf("%i \n", f_index) ;
-
-    if (f_index >= 1 && f_index <= 16)
-    {
-        printf("Grade %i\n", f_index) ;
-    }
-    else
-    {
-        if (index < 1)
-        {
-            printf("Before Grade 1\n") ;
-        }
-        if (f_index > 16)
-        {
-            printf("Grade 16+\n") ;
-        }
-    }
-
-
-//printf("Letter(s): %f \n", letters);
-//printf("Words(s): %f \n", words);
-//printf("Sentence(s): %f \n", sentences);
+	int i = 0;
+	char c = '\0';
+	while (text[i])
+	{
+	 	c = text[i];
+		if (c == '.' || c == '!' || c == '?')
+			sentences++;
+		i++;
+	}
+	return sentences;
 }
